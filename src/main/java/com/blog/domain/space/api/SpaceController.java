cb17c5d3d23717c8a9ffab6a7bf5a134af62b7a9
joohee <joo9837@naver.com>
2024-10-22 10:11:59 +0900

    Feat(space/api): SpaceController 구현
    - addSpace 구현

--- src/main/java/com/blog/domain/space/api/SpaceController.java
+++ src/main/java/com/blog/domain/space/api/SpaceController.java
@@ -1,4 +1,25 @@
 package com.blog.domain.space.api;
 
+import com.blog.domain.space.dto.SpaceReqDto;
+import com.blog.domain.space.dto.SpaceResDto;
+import com.blog.domain.space.service.SpaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/space")
+@RestController
 public class SpaceController {
+    private final SpaceService spaceService;
+
+    @PostMapping("")
+    public ResponseEntity<SpaceResDto> addSpace(@RequestBody SpaceReqDto spaceReqDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(spaceService.addSpace(spaceReqDto));
+    }
 }
