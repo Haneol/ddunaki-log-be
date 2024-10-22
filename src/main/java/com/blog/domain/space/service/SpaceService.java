75e9375a5a78ae061a8923ba6417d1457ec435e6
joohee <joo9837@naver.com>
2024-10-22 10:07:17 +0900

    Feat(space/service): SpaceService 구현
    - addSpace 구현
    - updateSpaceName 구현
    - updateSpaceDate 구현
    - updateSpaceCode 구현
    - updateSpaceDescription 구현
    - updateMaxMembers 구현
    - findAllSpaces 구현
    - findOneSpace 구현
    - deleteSpace 구현
    - addMember 구현
    - deleteMember 구현

--- src/main/java/com/blog/domain/space/service/SpaceService.java
+++ src/main/java/com/blog/domain/space/service/SpaceService.java
@@ -1,4 +1,19 @@
 package com.blog.domain.space.service;
 
+import com.blog.domain.space.dto.*;
+
+import java.util.List;
+
 public interface SpaceService {
+    SpaceResDto addSpace(SpaceReqDto spaceReqDto);
+    SpaceNameResDto updateSpaceName(SpaceNameReqDto spaceNameReqDto);
+    SpaceDateResDto updateSpaceDate (SpaceDateReqDto spaceDateReqDto);
+    SpaceCodeResDto updateSpaceCode (SpaceCodeReqDto spaceCodeReqDto);
+    SpaceDescriptionResDto updateSpaceDescription(SpaceDescriptionReqDto spaceDescriptionReqDto);
+    SpaceMaxMembersResDto updateMaxMembers(SpaceMaxMembersReqDto spaceMaxMembersReqDto);
+    List<SpaceResDto> findAllSpaces(int nationCode, int cityCode);
+    SpaceResDto findOneSpace(Long spaceId);
+    void deleteSpace(Long spaceId);
+    SpaceMembersResDto addMember(SpaceMembersReqDto spaceMembersReqDto);
+    SpaceMembersResDto deleteMember (SpaceMembersReqDto spaceMembersReqDto);
 }
