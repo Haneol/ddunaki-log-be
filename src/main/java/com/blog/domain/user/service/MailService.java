3fbde7836f0ca3ae81c6aff27f1f1f597fe89183
yunju Lee <76641389+shining013@users.noreply.github.com>
2024-10-23 23:45:40 +0900

    Revert "login and signup"

--- src/main/java/com/blog/domain/user/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package com.blog.domain.user.service;
-
-import javax.mail.internet.MimeMessage;
-
-public interface MailService {
-    void createNumber();
-    MimeMessage createMail(String email);
-    int sendMail(String email);
-}
