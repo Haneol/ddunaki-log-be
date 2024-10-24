0c8a33e5c3443e38e3f7c9d707a4d3d9ffeb6363
shining013 <nevertheless113@naver.com>
2024-10-25 03:26:15 +0900

    feat: 마지막 cors

--- src/main/java/com/blog/domain/user/config/CorsConfig.java
+++ src/main/java/com/blog/domain/user/config/CorsConfig.java
@@ -2,24 +2,32 @@ package com.blog.domain.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080","http://haneol-test.kro.kr"));
-        configuration.setAllowedMethods(List.of("HEAD","GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.addAllowedMethod(HttpMethod.GET);
+        configuration.addAllowedMethod(HttpMethod.POST);
+        configuration.addAllowedMethod(HttpMethod.HEAD);
+        configuration.addAllowedMethod(HttpMethod.PUT);
+        configuration.addAllowedMethod(HttpMethod.DELETE);
+        configuration.addAllowedMethod(HttpMethod.TRACE);
+        configuration.addAllowedMethod(HttpMethod.OPTIONS);
         configuration.setAllowedHeaders(List.of("Authorization","Cache-Control","Content-Type"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return new CorsFilter(source);
     }
 }
