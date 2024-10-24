c5d534b29ec8c0a7a5b89a55dc9cf2052c5376f7
joo9837 <joo9837@naver.com>
2024-10-24 22:27:47 +0900

    Feat(user): 멤버 dto email 추가

--- src/main/java/com/blog/domain/user/dto/MemberDto.java
+++ src/main/java/com/blog/domain/user/dto/MemberDto.java
@@ -11,12 +11,14 @@ public class MemberDto {
     private final Long userId;
     private final String nickname;
     private final String profile;
+    private final String email;
 
     public static MemberDto entityToDto(User user) {
         return MemberDto.builder()
                 .userId(user.getUserId())
                 .nickname(user.getNickName())
                 .profile(user.getProfile())
+                .email(user.getEmail())
                 .build();
     }
 }
